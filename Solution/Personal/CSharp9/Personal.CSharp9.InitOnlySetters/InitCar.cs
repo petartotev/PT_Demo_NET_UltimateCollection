a4b58c452c53bc49298f236f66d969af474c5e8b
Petar Totev <petar@petartotev.net>
2020-12-14 10:30:39 +0200

    Demo on Init only setters is now complete.

---
+++ Solution/Personal/CSharp9/Personal.CSharp9.InitOnlySetters/InitCar.cs
@@ -0,0 +1,14 @@
+﻿namespace Personal.CSharp9.InitOnlySetters
+{
+    public class InitCar
+    {
+        public InitCar()
+        {
+
+        }
+
+        public string Color { get; init; }
+
+        public int CountDoors { get; init; }
+    }
+}
--- Solution/Personal/CSharp9/Personal.CSharp9.InitOnlySetters/Program.cs
+++ Solution/Personal/CSharp9/Personal.CSharp9.InitOnlySetters/Program.cs
@@ -1,12 +1,17 @@
-﻿using System;
-
-namespace Personal.CSharp9.InitOnlySetters
+﻿namespace Personal.CSharp9.InitOnlySetters
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            InitCar myCar1 = new InitCar()
+            {
+                Color = "green",
+                CountDoors = 4,
+            };
+
+            // CS8852: Init-only property or indexer can only be assigned in an object initializer, or on 'this' or 'base' in an instance constructor or an 'init' accessor.
+            //myCar1.CountDoors = 5;
         }
     }
 }
