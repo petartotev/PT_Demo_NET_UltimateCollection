87498d464cd81d2730afc2a94f1d0a724fd5cefb
petartotev <petar@petartotev.net>
2021-12-21 21:50:44 +0200

    Encapsulation vs reflection project added

---
+++ src/Personal/General/Personal.General.EncapsulationVsReflection/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace Personal.General.EncapsulationVsReflection
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Textbook myTexbook = new Textbook();
+
+            MethodInfo myMethodInfo = myTexbook.GetType().GetMethod("Read", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            myMethodInfo.Invoke(myTexbook, null);
+        }
+    }
+}
---
+++ src/Personal/General/Personal.General.EncapsulationVsReflection/Textbook.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Personal.General.EncapsulationVsReflection
+{
+    public class Textbook
+    {
+        private void Read()
+        {
+            Console.WriteLine("Reading!");
+        }
+    }
+}
