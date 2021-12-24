4c2c73a5e9f3e8bfa10d20a4d715b184abf4c383
petartotev <petar@petartotev.net>
2021-12-24 13:58:58 +0200

    Used default! to neglect <nullable> error

--- src/Personal/General/Personal.General.NullableEnable/Person.cs
+++ src/Personal/General/Personal.General.NullableEnable/Person.cs
@@ -2,5 +2,17 @@
 {
     public class Person
     {
+        public string SomePropertyThatShouldNotBeNull { get; set; }
+
+        public Person()
+        {
+            // "Converting null literal or possible null value to non-nullable type" because of <Nullable>enable</Nullable>.
+            //this.SomePropertyThatShouldNotBeNull = null;
+
+            // "Converting null literal or possible null value to non-nullable type" because of <Nullable>enable</Nullable>.
+            //this.SomePropertyThatShouldNotBeNull = default;
+
+            this.SomePropertyThatShouldNotBeNull = default!;
+        }
     }
 }
