310a911622fec618fb6d46eaa4f8993468aead2d
petartotev <petar@petartotev.net>
2024-01-18 21:04:22 +0200

    CSharp9 demo removed and moved as NET5 demo in PT_Demo_NET Solution

--- src/Personal/CSharp9/Personal.CSharp9.Records/RecordChild.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Personal.CSharp9.Records
-{
-    public record RecordChild : RecordPerson
-    {
-        public string Toy { get; }
-
-        public RecordChild(string firstName, string lastName, string toy) : base(firstName, lastName) => Toy = toy;
-    }
-}
--- src/Personal/CSharp9/Personal.CSharp9.TopLevelStatements/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// #3 Top-level statements
-
-using System;
-
-string message = "Top-level statements remove unnecessary ceremony from many applications.\n" +
-    "There’s only one line of code that does anything. With top-level statements, \n" +
-    "you can replace all that boilerplate with the using statement and the single line that does the work.";
-
-Console.WriteLine(message);
